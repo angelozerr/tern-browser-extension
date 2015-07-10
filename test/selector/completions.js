9a5d4cbff5d4a97f65a79d151c333be468ad4f7c
angelozerr <angelo.zerr@gmail.com>
2015-07-10 17:13:53 +0200

    Completion for HTML elements

--- test/selector/completions.js
+++ test/selector/completions.js
@@ -4,14 +4,9 @@ var util = require("../util");
 
 exports['test querySelector completion'] = function() {
   
-  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector(''", {
-    "start":{"line":0,"ch":59},
-    "end":{"line":0,"ch":61},
-    "isProperty":false,
-    "isObjectKey":false,
-    "completions":[{"name":"'#MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
-                  ]
-  }, null, 1);
+  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector(''",
+    {"name":"'#MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+  , "'#MyInput'", 1);
   
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('#'", {
     "start":{"line":0,"ch":59},
@@ -27,7 +22,12 @@ exports['test querySelector completion'] = function() {
     "end":{"line":0,"ch":62},
     "isProperty":false,
     "isObjectKey":false,
-    "completions":[{"name":"'#MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+    "completions":[{"name":"'#MyInput'","type":"Attr","origin":"test1.html","displayName":"#MyInput"},
+                   {"name":"'map'","type":"HTMLMapElement","origin":"test1.html","displayName":"map"},
+                   {"name":"'mark'","type":"HTMLMarkElement","origin":"test1.html","displayName":"mark"},
+                   {"name":"'menu'","type":"HTMLMenuElement","origin":"test1.html","displayName":"menu"},
+                   {"name":"'meta'","type":"HTMLMetaElement","origin":"test1.html","displayName":"meta"},
+                   {"name":"'meter'","type":"HTMLMeterElement","origin":"test1.html","displayName":"meter"}
     ]
   }, null, 1);
 
@@ -44,14 +44,9 @@ exports['test querySelector completion'] = function() {
 
 exports['test querySelector completion with before content'] = function() {
   
-  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div '", {
-    "start":{"line":0,"ch":59},
-    "end":{"line":0,"ch":65},
-    "isProperty":false,
-    "isObjectKey":false,
-    "completions":[{"name":"'div #MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
-                  ]
-  }, null, 1);
+  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div '", 
+      {"name":"'div #MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+  , "'div #MyInput'", 1);
   
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div #'", {
     "start":{"line":0,"ch":59},
@@ -67,8 +62,13 @@ exports['test querySelector completion with before content'] = function() {
     "end":{"line":0,"ch":66},
     "isProperty":false,
     "isObjectKey":false,
-    "completions":[{"name":"'div #MyInput'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
-    ]
+    "completions":[{"name":"'div #MyInput'","type":"Attr","origin":"test1.html","displayName":"#MyInput"},
+                   {"name":"'div map'","type":"HTMLMapElement","origin":"test1.html","displayName":"map"},
+                   {"name":"'div mark'","type":"HTMLMarkElement","origin":"test1.html","displayName":"mark"},
+                   {"name":"'div menu'","type":"HTMLMenuElement","origin":"test1.html","displayName":"menu"},
+                   {"name":"'div meta'","type":"HTMLMetaElement","origin":"test1.html","displayName":"meta"},
+                   {"name":"'div meter'","type":"HTMLMeterElement","origin":"test1.html","displayName":"meter"}
+                  ]
   }, null, 1);
 
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div X'", {
@@ -84,14 +84,9 @@ exports['test querySelector completion with before content'] = function() {
 
 exports['test querySelector completion with after content'] = function() {
   
-  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector(' div'", {
-    "start":{"line":0,"ch":59},
-    "end":{"line":0,"ch":65},
-    "isProperty":false,
-    "isObjectKey":false,
-    "completions":[{"name":"'#MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
-                  ]
-  }, null, 5);
+  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector(' div'",
+    {"name":"'#MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+  , "'#MyInput div'", 5);
   
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('# div'", {
     "start":{"line":0,"ch":59},
@@ -107,10 +102,15 @@ exports['test querySelector completion with after content'] = function() {
     "end":{"line":0,"ch":66},
     "isProperty":false,
     "isObjectKey":false,
-    "completions":[{"name":"'#MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+    "completions":[{"name":"'#MyInput div'","type":"Attr","origin":"test1.html","displayName":"#MyInput"},
+                   {"name":"'map div'","type":"HTMLMapElement","origin":"test1.html","displayName":"map"},
+                   {"name":"'mark div'","type":"HTMLMarkElement","origin":"test1.html","displayName":"mark"},
+                   {"name":"'menu div'","type":"HTMLMenuElement","origin":"test1.html","displayName":"menu"},
+                   {"name":"'meta div'","type":"HTMLMetaElement","origin":"test1.html","displayName":"meta"},
+                   {"name":"'meter div'","type":"HTMLMeterElement","origin":"test1.html","displayName":"meter"}
     ]
   }, null, 5);
-
+  
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('X div'", {
     "start":{"line":0,"ch":59},
     "end":{"line":0,"ch":66},
@@ -124,14 +124,9 @@ exports['test querySelector completion with after content'] = function() {
 
 exports['test querySelector completion with before+after content'] = function() {
   
-  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div  div'", {
-    "start":{"line":0,"ch":59},
-    "end":{"line":0,"ch":69},
-    "isProperty":false,
-    "isObjectKey":false,
-    "completions":[{"name":"'div #MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
-                  ]
-  }, null, 5);
+  util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div  div'",
+   {"name":"'div #MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}   
+  , "'div #MyInput div'", 5);
   
   util.assertCompletion("<html><input id='MyInput' /><script>document.querySelector('div # div'", {
     "start":{"line":0,"ch":59},
@@ -147,7 +142,12 @@ exports['test querySelector completion with before+after content'] = function() 
     "end":{"line":0,"ch":70},
     "isProperty":false,
     "isObjectKey":false,
-    "completions":[{"name":"'div #MyInput div'","type": "Attr", "origin": "test1.html", "displayName":"#MyInput"}
+    "completions":[{"name":"'div #MyInput div'","type":"Attr","origin":"test1.html","displayName":"#MyInput"},
+                   {"name":"'div map div'","type":"HTMLMapElement","origin":"test1.html","displayName":"map"},
+                   {"name":"'div mark div'","type":"HTMLMarkElement","origin":"test1.html","displayName":"mark"},
+                   {"name":"'div menu div'","type":"HTMLMenuElement","origin":"test1.html","displayName":"menu"},
+                   {"name":"'div meta div'","type":"HTMLMetaElement","origin":"test1.html","displayName":"meta"},
+                   {"name":"'div meter div'","type":"HTMLMeterElement","origin":"test1.html","displayName":"meter"}
     ]
   }, null, 5);
 
